a6eda12899fc5830d2d9f7824f2498c99b494224
pororoJ <walkslowlyjia@gmail.com>
2019-01-30 23:58:55 +0800

    feat: add redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const {students:{name}} = this.props
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <code>{name}</code>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { students } = state;
+  return { students };
+};
+
+export default connect(mapStateToProps)(App);
---
+++ src/redux/actionTypes.js
@@ -0,0 +1,1 @@
+export const LOAD_STUDENTS = "LOAD_STUDENTS";
